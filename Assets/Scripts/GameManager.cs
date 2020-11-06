faac367c7846ffedd7092dcdbc601c3405066a12
ClaXXX <claire.bouttes@gmail.com>
2020-11-06 15:54:09 +0100

    UI menus

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Play()
+    {
+        Debug.Log("Game Started");
+    }
+
+    public void LaunchGame()
+    {
+        Debug.Log("Game Launched");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Game Preferences Menu open");
+    }
+
+    /**
+     * Potentially useless and to replace by Restart Game
+     */
+    public void BackToMainMenu()
+    {
+        Debug.Log("Back To Main Menu");
+    }
+
+    public void BackToGame()
+    {
+        Debug.Log("Back To Game");
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Game Restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void Quit()
+    {
+        Debug.Log("Game Application Ended");
+        Application.Quit();
+    }
+}
+

21a3da54394321fc52d034a8b6bc83a91bce67d3
Philippe <tiphil1@live.ca>
2020-12-17 16:51:12 -0500

    pooled music notes

---
+++ Assets/Scripts/Misc/Radio.cs
@@ -0,0 +1,19 @@
+﻿using Particles;
+using UnityEngine;
+
+public class Radio : MonoBehaviour {
+    public ParticleSystemPool activePlayerParticlesSystemPool;
+    // Start is called before the first frame update
+    void Start() {
+        // TODO : Get system de particules
+        var test = activePlayerParticlesSystemPool.GetFromPool(this.transform);
+        test.rotation = Quaternion.Euler(90, 0, 0);
+        //particle.GetComponent<PooledParticleSystem>().startParticleSystem(activePlayerParticlesSystemPool);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

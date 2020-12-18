98c32796bea0e4bdccde669231250952b06bacd0
Philippe <tiphil1@live.ca>
2020-12-18 14:19:47 -0500

    début rework intergace

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -1,5 +1,7 @@
-﻿using DefaultNamespace;
+﻿using System;
+using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -11,6 +13,28 @@ namespace UI {
         [SerializeField] private Dropdown localPlayer2Controls;
         [SerializeField] private Dropdown player2Select;
 
+        #region UIGameObjects
+
+        [SerializeField] private GameObject LowerLocalGameSetterGameObject;
+        [SerializeField] private GameObject UpperLocalGameSetterGameObject;
+        // TODO : Add main menu thing here
+
+        #endregion
+        
+        #region VisibleCoordinates
+
+        [SerializeField] private Transform LowerLocalGameSetterVisiblePosition;
+        [SerializeField] private Transform UpperLocalGameSetterVisiblePosition;
+        // TODO : Add main menu things here
+
+        #endregion
+        
+
+        public bool toMainMenu;
+        public bool toGameConfig;
+        public bool toGame;
+        
+        
         private PlayerAssembler playerAssembler;
         private ConfigManager _configManager;
         
@@ -19,6 +43,25 @@ namespace UI {
         public void Start() {
             playerAssembler = new PlayerAssembler();
             _configManager = ConfigManager.instance;
+        }
+
+        public void Update() {
+            float time = Time.deltaTime;
+            if (toMainMenu) {
+                // TODO : Implement
+            }
+
+            if (toGameConfig) {
+                easeOutMainMenu(time);
+                // TODO : Une fois le main menu "out", on ease "in" le play menu
+                if (true) {
+                    easeInPlayPage(time);
+                }
+            }
+
+            if (toGame) {
+                
+            }
         }
 
         public void quitGame() {
@@ -78,6 +121,49 @@ namespace UI {
             else {
                 localPlayer2Controls.value = 1;
             }
+        }
+
+        public void setToMainMenu(bool boolean) {
+            toMainMenu = boolean;
+        }
+        
+        public void setToPlayMenu(bool boolean) {
+            toGameConfig = boolean;
+        }
+        
+        private void easeOutMainMenu(float time) {
+            // TODO : Mth that is call to move menu items    
+        }
+
+        private void easeOutPlayPage(float time) {
+            // TODO : Mth that is call to move menu items   
+        }
+
+        private void easeInPlayPage(float time) {
+            Vector3 test =
+            interpolate(UpperLocalGameSetterGameObject.transform.position,
+                UpperLocalGameSetterVisiblePosition.position, time);
+
+            UpperLocalGameSetterGameObject.transform.position = test;
+            Debug.Log(UpperLocalGameSetterGameObject.transform.position.x - 
+                      UpperLocalGameSetterVisiblePosition.position.x);
+            if (UpperLocalGameSetterGameObject.transform.position.y - 0.1f < UpperLocalGameSetterVisiblePosition.position.y) {
+                UpperLocalGameSetterGameObject.transform.position = UpperLocalGameSetterVisiblePosition.position;
+                toGameConfig = false;
+            }
+        }
+
+        private void makeButtonRainbow(float time) {
+            // TODO : Mth that is called to interpolate button color
+        }
+        private Vector3 interpolate(Vector3 a, Vector3 b, float t) {
+            t = Mathf.Clamp01(t); 
+            
+            t = (2f * t) - (2f * t * t);
+
+            return new Vector3(t * b.x + (1 - t) * a.x,
+                t * b.y + (1 - t) * a.y, 
+                t * b.z + (1 - t) * a.z);
         }
     }
 }

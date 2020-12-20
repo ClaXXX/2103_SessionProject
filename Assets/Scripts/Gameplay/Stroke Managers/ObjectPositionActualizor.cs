8cede2d93f6ef73e360b6dde2845e657ad716565
ClaXXX <claire.bouttes@gmail.com>
2020-12-20 23:42:44 +0100

    Animation speed fix

--- Assets/Scripts/Gameplay/Stroke Managers/ObjectPositionActualizor.cs
+++ Assets/Scripts/Gameplay/Stroke Managers/ObjectPositionActualizor.cs
@@ -53,7 +53,7 @@ namespace Gameplay.Stroke_Managers
                     _descendant = false;
                 else if (x > 1f)
                     _descendant = true;
-                x = _descendant ? x - 0.05f * Time.deltaTime : x + 0.05f * Time.deltaTime;
+                x = _descendant ? x - Time.deltaTime : x + Time.deltaTime;
             }
             
             switch (effect)

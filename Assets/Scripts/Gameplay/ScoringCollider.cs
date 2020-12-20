b214aecff89711639848fd06588f26b2cff65477
ClaXXX <claire.bouttes@gmail.com>
2020-12-20 23:31:04 +0100

    rajcthcltcrt

--- Assets/Scripts/Gameplay/ScoringCollider.cs
+++ Assets/Scripts/Gameplay/ScoringCollider.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using GamePlay;
+using Particles;
 using Sounds;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ScoringCollider : MonoBehaviour {
     private GameObject soundObject;
     private SoundManager soundManager;
+    public ParticleSystemPool particleSystemPool;
     public Action OnGameWin;
-
+    
     public void Awake() {
         soundObject = GameObject.Find("SoundManager");
         soundManager = soundObject.GetComponent<SoundManager>();
+        particleSystemPool = FindObjectOfType<ParticleSystemPool>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,6 +28,9 @@ public class ScoringCollider : MonoBehaviour {
         soundManager.playBallInHoleSound(this.transform);
 
         yield return new WaitForSeconds(1);
+        particleSystemPool.GetFromPool(transform);
+        var test = particleSystemPool.GetComponent<PooledParticleSystem>();
+        test.startParticleSystem(particleSystemPool);
         
         OnGameWin?.Invoke();
     }

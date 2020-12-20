5ad168e4c6f5fb7cf100e0c1d97da96d40eba576
ClaXXX <claire.bouttes@gmail.com>
2020-12-20 11:19:00 +0100

    [Structure] Restructration of ressources: Material, Sounds and all Resources folders under the Resources Folder

---
+++ Assets/Scripts/Gameplay/Stroke Managers/ObjectPositionActualizor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Gameplay.Stroke_Managers
+{
+    public class ObjectPositionActualizor: MonoBehaviour
+    {
+        public GameObject mainObject;
+        private Vector3 _offset;
+
+        void Start()
+        {
+            _offset = transform.position - mainObject.transform.position;
+        }
+
+        void Update()
+        {
+            transform.position = mainObject.transform.position + (transform.rotation * _offset);
+        }
+    }
+}
